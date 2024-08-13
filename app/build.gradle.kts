1f2b48f179a70b96ce6ff6f5e2987c615e35d7a4
pixelh_97 <abakhet2010@gmail.com>
2024-08-13 13:32:41 +0300

    clearing project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,7 +56,6 @@ dependencies {
     implementation(libs.androidx.navigation.fragment.ktx)
     implementation(libs.androidx.navigation.ui.ktx)
     implementation(libs.androidx.legacy.support.v4)
-    implementation(project(":domain"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
--- data/build.gradle.kts
+++ data/build.gradle.kts
@@ -37,7 +37,6 @@ dependencies {
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
-    implementation(project(":domain"))
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
